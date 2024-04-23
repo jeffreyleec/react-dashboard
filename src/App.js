4bc002a52ca28dfc61bd72cb251b775cf0bf0745
Jeffrey <jeffrey-lee@live.ca>
2024-04-23 11:13:35 -0700

    theme context and theme provider added to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-function App() {
-  return (
-    <div classname="App">
+import { ColorModeContext, useMode } from './theme'
+import { CssBaseline, ThemeProvider } from '@mui/material'
 
-    </div>
+function App() {
+  const [theme, colorMode] = useMode()
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="App">
+          <main className='content'></main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
